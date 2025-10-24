cd7aef076baf7044c4fdd37206a73b69774d4b3b
pinky <pinkipandarojo@gmail.com>
2025-10-24 13:16:28 -0400

    Fix-234

--- backend/src/modules/repository_documents/test/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import '@nestjs/testing';
-
-// No direct import of '@types/jest' (types-only) - TypeScript will pick up Jest types
-// via tsconfig "types" or the test tsconfig. Avoid importing 'jest' here.
-jest.setTimeout(10000);
